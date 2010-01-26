c9b39dd9e844a130bf06e9d5d31f286820dbc9f7
Matthew Foemmel <git@foemmel.com>
2010-01-25 21:40:38 -0600

    updated help

--- commands/help.rb
+++ commands/help.rb
@@ -9,11 +9,14 @@ delete-project <project>
 
 Repositories:
 
+list-repositories <project>
 create-repository <project> <repository>
 delete-repository <project> <repository>
+fork-repository <from-project> <from-repository> <to-project> <to-repository>
 
 Permissions:
 
+list-permissions <project>
 add-permission <project> <username> read|write|admin
 remove-permission <project> <username>
 
