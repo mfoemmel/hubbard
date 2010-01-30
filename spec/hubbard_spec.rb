8ad9ef40caa6ecf8f0a54dec14ba4bdd7ff50698
Matthew Foemmel <git@foemmel.com>
2010-01-30 13:40:49 -0600

    added set-description and set-visibility commands

--- spec/hubbard_spec.rb
+++ spec/hubbard_spec.rb
@@ -12,6 +12,20 @@ describe "Hubble" do
     projects.should == ["foo"]
   end
 
+  it "should set project description" do
+    hub("kipper", "create-project foo old-desc")
+    hub("kipper", "set-description foo new-desc")
+    project = hub("kipper", "list-projects").split("\n")[0].split
+    project[2].should == "new-desc"
+  end
+
+  it "should set project visibility" do
+    hub("kipper", "create-project foo foo-desc")
+    hub("kipper", "set-visibility foo private")
+    project = hub("kipper", "list-projects").split("\n")[0].split
+    project[1].should == "private"
+  end
+
   it "should not allow multiple projects with same name" do
     hub("kipper", "create-project foo foo-desc")
     lambda { hub("kipper", "create-project foo") }.should raise_error
