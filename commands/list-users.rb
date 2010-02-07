53435956ddbbb2a57a00fb289da7d6e0730feb69
Brian Tatnall <btatnall@gmail.com>
2010-02-07 01:24:57 -0600

    Fix syntax error in list-users.

--- commands/list-users.rb
+++ commands/list-users.rb
@@ -1,9 +1,9 @@
 if @username != 'admin'
-    $stderr.puts "You don't have permission to do that"
-    exit 3
+  $stderr.puts "You don't have permission to do that"
+  exit 3
 end
 
-Dir.entries(Hubbard::ACCOUNTS_PATH)).each do |account|
+Dir.entries(Hubbard::ACCOUNTS_PATH).each do |account|
   next if account == '.' || account == '..'
   puts account
 end
