51d57bf14a2df1498ecccb7211afbec2c8c1aec8
Brian Tatnall <btatnall@gmail.com>
2010-02-06 18:23:01 -0600

    Adding yaml output for permissions.

--- commands/list-permissions.rb
+++ commands/list-permissions.rb
@@ -3,12 +3,24 @@ authorize(project_name, 'admin')
 dir = find_project_dir(project_name)
 username = ARGV.shift
 
+contents = ""
 permissions_file = File.join(dir, ".permissions")
 if File.exists?(permissions_file)
   File.open(permissions_file, "r+") do |f|
     f.flock(File::LOCK_EX)
     contents = f.read
-    puts contents
     f.flock(File::LOCK_UN)
   end
+end
+
+if OPTIONS[:format] == :yaml
+  permissions = []
+  contents.split("\n").map do |l|
+    l.strip!
+    p = l.split('=')
+    permissions << { :user => p.first, :access => p.last }
+  end
+  puts YAML::dump(permissions)
+elsif OPTIONS[:format] == :text
+  puts contents unless contents.empty?
 end
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -11,7 +11,7 @@ describe "Hubble with yaml output" do
     reset_file_system
   end
 
-  it "should load list-projects" do
+  it "should list-projects" do
     hub("yammer", "create-project a a-desc")
     hub("yammer", "create-project b b-desc")
     hub("yammer", "create-project c c-desc")
@@ -20,7 +20,7 @@ describe "Hubble with yaml output" do
     projects.should == ["a", "b", "c"]
   end
 
-  it "should create repositories" do
+  it "should list repositories" do
     hub("yammer", "create-project a a-desc")
     hub("yammer", "create-repository a b")
 
@@ -28,5 +28,13 @@ describe "Hubble with yaml output" do
     repositories.length.should == 1
     repositories.first[:name].should == "b"
     repositories.first[:url].should == "#{ENV['USER']}@#{HUB_HOST}:a/b.git"
+  end
+
+  it "should list permissions" do
+    hub("yammer", "create-project a a-desc")
+    permissions = YAML::load(hub("yammer", "#{YAML_OPTION} list-permissions a"))
+    permissions.length.should == 1
+    permissions.first[:user].should == "yammer"
+    permissions.first[:access].should == "admin"
   end
 end
