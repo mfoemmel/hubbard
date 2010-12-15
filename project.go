a84019e07f80e122cc988e342ff62332f413652d
Matthew Foemmel <git@foemmel.com>
2010-12-15 17:00:30 -0600

    added project.go

---
+++ project.go
@@ -0,0 +1,12 @@
+package hubbard
+
+import "path"
+
+type project struct {
+	name string
+}
+
+func (self *project) getLogFile(sha1 string) string {
+	return path.Join("data", "build", self.name, sha1 + ".log")
+
+}
